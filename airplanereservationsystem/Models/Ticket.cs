39e99df45babcd5dd647fcc4d4fb273f0fe280d9
bnguy024 <brandon.nguyen1@ucalgary.ca>
2021-04-18 23:26:12 -0600

    Fixed ticket api

--- airplanereservationsystem/Models/Ticket.cs
+++ airplanereservationsystem/Models/Ticket.cs
@@ -10,14 +10,14 @@ namespace airplanereservationsystem.Models
             Provides = new HashSet<Provides>();
         }
 
-        public string Amount { get; set; }
-        public string ClassType { get; set; }
+        public int TicketNumber { get; set; }
+        public int ReservationSystemId { get; set; }
         public string Date { get; set; }
+        public string SeatNum { get; set; }
+        public string ClassType { get; set; }
+        public string Amount { get; set; }
         public string DateIssued { get; set; }
         public int? LegNum { get; set; }
-        public int ReservationSystemId { get; set; }
-        public int? SeatNum { get; set; }
-        public int TicketNumber { get; set; }
 
         public virtual ReseverationSystem ReservationSystem { get; set; }
         public virtual ICollection<Provides> Provides { get; set; }
